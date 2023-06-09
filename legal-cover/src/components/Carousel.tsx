e390b131ee7058c2fac61c77474100631f31bb14
suraj-v <suraj.verma@techx-consulting.com>
2023-06-09 21:34:20 +0530

    Carousel added

--- legal-cover/src/components/Carousel.tsx
+++ legal-cover/src/components/Carousel.tsx
@@ -1,7 +1,8 @@
 import Slider from 'react-slick';
-import { Card, CardContent, Typography, Container } from '@mui/material';
+import { Card, CardContent, Typography, Container, Rating } from '@mui/material';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import '../styles/CarouselComponent.css'
 
 function CarouselComponent() {
     const items = [
@@ -20,6 +21,11 @@ function CarouselComponent() {
           description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
           image: 'image3.jpg',
         },
+        {
+          title: 'June 7, 2023',
+          description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+          image: 'image3.jpg',
+        },
       ];
     
       const settings = {
@@ -27,7 +33,7 @@ function CarouselComponent() {
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 3000,
         responsive: [
           {
@@ -46,11 +52,13 @@ function CarouselComponent() {
       };
     
       return (
-        <Container maxWidth="md" sx={{ marginTop: '5rem' }}>
+      <Container maxWidth="md" sx={{ marginTop: '5rem',
+                                       marginBottom: '5rem' }}>
+       <Typography variant="h5" textAlign="center"sx={{ marginTop: '5rem',marginBottom: '5rem' }} >What our users say</Typography>
           <Slider {...settings}>
             {items.map((item, index) => (
-              <Card key={index}>
-                <img src={item.image} alt="Card image cap" />
+              <Card key={index} style={{ margin: '0 160px' }}>
+               <Rating name="size-small" defaultValue={5} size="small" />
                 <CardContent>
                   <Typography variant="h5" component="h2">
                     {item.title}
